940c7515e8afa1a38525d0397d3d3bc4cc9d6bd0
edunicastro <edunicastro@users.noreply.github.com>
2019-09-11 02:01:41 -0300

    Adicionado AhpForm com um formulário em etapas

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App AAWWWWS"/>
+    <AhpForm />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AhpForm from './components/AhpForm.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    AhpForm
   }
 }
 </script>
---
+++ src/components/StepThree.vue
@@ -0,0 +1,50 @@
+<template>
+    <div style="padding: 2rem 3rem; text-align: left;">
+        <div class="field">
+            <label class="label">Categorias</label>
+            <div class="control">
+                <input-tag v-model="form.categorias" placeholder="Escreva uma categoria e pressione enter"></input-tag>
+            </div>
+        </div>
+        <div>
+            <p>
+                Placeholder: Aqui haverá uma lista com checkbox para ativar ou desativar cada Alternativa
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['clickedNext', 'currentStep'],
+    data() {
+        return {
+            form: {
+                categorias: []
+            }
+        }
+    },
+    watch: {
+        form: {
+            handler: function (obj) {
+                console.log('obj')
+                console.log(obj)
+                for (let key in obj) {
+                    if (!obj.hasOwnProperty(key) || obj[key].length <= 0) {
+                        this.$emit('can-continue', {value: false})
+                        return
+                    }
+                }
+                this.$emit('can-continue', {value: true})
+            },
+            deep: true
+        },
+        clickedNext(val) {
+            if(val === true) {
+                this.$v.form.$touch();
+            }
+        }
+    }
+}
+</script>
