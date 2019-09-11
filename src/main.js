ed314ae6fda270e7c4daa2308a05acfe841e97e6
edunicastro <edunicastro@users.noreply.github.com>
2019-09-11 02:02:40 -0300

    Dependência/alteração necessária para funcionar o componente de tags. Adicionado dessa forma para funcionar global já que será usado em vários componentes.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import InputTag from 'vue-input-tag'
 
 Vue.config.productionTip = false
+
+Vue.component('input-tag', InputTag)
 
 new Vue({
   render: h => h(App),
