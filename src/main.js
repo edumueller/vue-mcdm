1b29c7afc0875ea12544b0770a58f6246083b598
eduardo <eduardo@logcomex.com>
2019-09-23 18:19:42 -0300

    Added Vuex for state management and a form key to state.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import InputTag from 'vue-input-tag'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    form: {}
+  }
+})
 
 Vue.component('input-tag', InputTag)
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
