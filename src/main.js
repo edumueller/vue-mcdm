053133982b200d04b69aa13ca1d8981d2859c1cc
edunicastro <edunicastro@users.noreply.github.com>
2019-09-29 17:19:52 -0300

    VueSlider import that was missing from previous commit. There is also some mapMutations scaffolding and necessary state configs that need to be in main.js

--- src/main.js
+++ src/main.js
@@ -2,17 +2,30 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App.vue'
 import InputTag from 'vue-input-tag'
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/default.css'
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    form: {}
+    form: {
+      categories: [],
+      alternatives: []
+    }
+  },
+  mutations: {
+    updateForm (state, payload) {
+      for (let k in payload) {
+      	state.form[k] = payload[k]
+      }
+    }
   }
 })
 
 Vue.component('input-tag', InputTag)
+Vue.component('VueSlider', VueSlider)
 
 new Vue({
   store,
